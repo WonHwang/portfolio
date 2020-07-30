c7a9533bca5b3a7da65a6ba771aee735b70f0797
ediblepotato <ediblepotato@likelion.org>
2020-07-30 19:26:57 +0900

    The end

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,6 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+'''
 with open('secret.json', 'r') as f:
     secret = json.loads(f.read())
 
@@ -22,6 +23,8 @@ def get_secret(setting, secret=secret):
 
 
 SECRET_KEY = get_secret('SECRET_KEY')
+'''
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
